a47c7f8c756ceb01e056dae09c41a90e23648602
unknown <amoahnkm@gmail.com>
2024-09-21 13:00:07 +0000

    eslint disabled actually

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = {
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-  };
-
-
-export default nextConfig;
+module.exports = nextConfig;
