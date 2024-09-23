797c6ed26506db110606326fa11e3558e6a6e3f8
unknown <amoahnkm@gmail.com>
2024-09-23 19:52:05 +0000

    fixing errors in updater

--- app/api/webhooks/paystack/route.ts
+++ app/api/webhooks/paystack/route.ts
@@ -5,7 +5,7 @@ import { findBooking, updateBookingStatus } from '@/lib/actions/book.action';
 import { updateSeats } from '@/lib/actions/bus.action';
 import Booking from '@/lib/database/models/booking.model';
 
-const secret = process.env.SECRET_KEY!;
+const secret = process.env.PASTACK_SECRET_KEY!;
 
 export async function POST(req: Request) {
   try {
@@ -21,13 +21,13 @@ export async function POST(req: Request) {
     if (hash === req.headers.get('x-paystack-signature')) {
       // Process the event here
       if(body.event==="charge.success"){
-        // const {reference, busId, seats} = await findBooking(body.data.reference)
-        // const updatedBooking = await updateBookingStatus(reference)
-        // await updateSeats({busId:busId, seatsToBook: seats.split(",")})
-        // console.log(updatedBooking)
+        const {reference, busId, seats} = await findBooking(body.data.reference)
+        const updatedBooking = await updateBookingStatus(reference)
+        await updateSeats({busId:busId, seatsToBook: seats.split(",")})
+        console.log(updatedBooking)
        
       }
-      await Booking.findOneAndUpdate({reference:"3f9on44wn4"}, {seats:body.json.stringify()})
+      //await Booking.findOneAndUpdate({reference:"3f9on44wn4"}, {seats:body.json.stringify()})
       console.log('Paystack event:', body);
 
       // Return a success response
