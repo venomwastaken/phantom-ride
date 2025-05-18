0409af4d4aaab4a099fcb50c6c86221e3d0b76ce
unknown <109553049+venomwastaken@users.noreply.github.com>
2025-05-18 07:03:38 +0000

    updated agent list, nana banyin

--- components/BookForm.tsx
+++ components/BookForm.tsx
@@ -341,7 +341,7 @@ export default function BookForm({ pickup, date}: bookFormProps) {
                     <Dropdown
                       onChangeHandler={field.onChange}
                       value={field.value}
-                      items={["Daniel", "Thelma", "Derrick", "Rodolphe", "Desmond", "Palba"]}
+                      items={["Daniel", "Thelma", "Derrick", "Rodolphe", "Desmond", "Palba", "Nana Banyin"]}
                       placeholder="Agent"
                       disabled={isSubmitting} // Disable during submission
                     />
