d18aabeadf54a3b18618d85317a144549de73c79
unknown <109553049+venomwastaken@users.noreply.github.com>
2025-08-09 17:34:18 +0000

    removed new public key processing

--- lib/actions/paymentPayaza.actions.ts
+++ lib/actions/paymentPayaza.actions.ts
@@ -30,10 +30,8 @@ export async function initializePayment({price, phoneNumber, networkBankCode, em
         })
 
     
-    const key = process.env.PAYAZA_PUBLIC_KEY!
+    const publicKey = process.env.PAYAZA_PUBLIC_KEY!
     
-    //convert public key to base64
-    const publicKey = Buffer.from(key).toString('base64');
 
     if (!publicKey) {
         throw new Error('Missing Payaza public key.');
